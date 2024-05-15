7da395be21f18c47bcb909ca8029716f81dcaa28
sagar-parida <paridag321@gmail.com>
2024-05-15 23:35:02 +0530

    Initial setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import PostCreateForm from "./PostCreateForm";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreateForm />
+    </div>
+  );
+}
+
+export default App;
